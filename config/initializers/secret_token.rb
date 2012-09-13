50d88e78f4a058d67886c21c0d44f2aa98a1390f
Nick Chadwick <nick@zencoder.com>
2012-09-13 16:32:47 -0700

    Init.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dreamlive::Application.config.secret_token = '9f8c3668b43489d4fed88d5487593b8dec4360f10abf050e18fd24a029aada6297c3b944847eef92b1c3378adce757b1e2c296c751b6e0ef7e0886036652d9b8'
