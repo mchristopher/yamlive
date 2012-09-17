e931b52eff261676186df2963328007e2aa42055
Nick Chadwick <nick@zencoder.com>
2012-09-16 21:25:25 -0700

    Fixed a bug in the migration adding users.

--- db/migrate/20120901000001_devise_create_users.rb
+++ db/migrate/20120901000001_devise_create_users.rb
@@ -1,7 +1,7 @@
 class DeviseCreateUsers < ActiveRecord::Migration
   def self.up
     create_table(:users) do |t|
-      t.string :username :null => false
+      t.string :username, :null => false
       t.database_authenticatable :null => false
       t.recoverable
       t.rememberable
