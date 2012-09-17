ddd943cdbd12a25004b9789a2835a6066fa365fe
Nick Chadwick <nick@zencoder.com>
2012-09-16 21:38:47 -0700

    Fixing codescool config.

--- config/initializers/zencoder.rb
+++ config/initializers/zencoder.rb
@@ -4,9 +4,9 @@ Dreamlive::Application.config.live_config = {
   :rtmp_publish => 'rtmp://codescool.com/live',
   :rtmp_out     => 'rtmp://codescool.com/live',
   :rtmp_append  => '',
-  :hls_publish  => 'ftp://testuser:testpwd@codescool.com/outputs',
+  :hls_publish  => 'ftp://testuser:testpwd@codescool.com/home/testuser/outputs',
   :hls_out      => 'http://codescool.com/outputs',
-  :file_out     => 'ftp://testuser:testpwd@codescool.com/outputs',
+  :file_out     => 'ftp://testuser:testpwd@codescool.com/home/testuser/outputs',
   :notify_url   => 'http://dreamlive.tv/streams'
 }
 
