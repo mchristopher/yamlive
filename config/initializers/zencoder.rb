3a94f675e31ade32632d204431f59a54c2be0681
Mike Christopher <mchristopher@users.noreply.github.com>
2014-10-07 19:26:36 -0700

    Update zencoder.rb

--- config/initializers/zencoder.rb
+++ config/initializers/zencoder.rb
@@ -1,13 +1,13 @@
-Zencoder.api_key = '79784bcdfd41a2e6f7af0defa88ffdbe'
+Zencoder.api_key = 'FILL_IN_API_KEY'
 
 Dreamlive::Application.config.live_config = {
-  :rtmp_publish => 'rtmp://brightcove-289:hfsbef@fmspush.iad.llnw.net/brightcove/289',
-  :rtmp_out     => 'rtmp://brightcove.fc.llnwd.net/brightcove/289',
+  :rtmp_publish => 'rtmp://my.rtmp.server',
+  :rtmp_out     => 'rtmp://my.rtmp.server',
   :rtmp_append  => '',
-  :hls_publish  => 'http://post.bcoveliveios-i.akamaihd.net/204874/1769255312001/yamlive',
-  :hls_out      => 'http://bcoveliveios-i.akamaihd.net/hls/live/204874/1769255312001/yamlive',
-  :file_publish => 's3://zencoder-video-outputs/yamlive/prod',
-  :file_out     => 'http://zencoder-video-outputs.s3.amazonaws.com/yamlive/prod',
-  :notify_url   => 'http://yamlive.mchristopher.com/streams'
+  :hls_publish  => 'http://my.hls.server',
+  :hls_out      => 'http://my.hls.server',
+  :file_publish => 's3://my-bucket',
+  :file_out     => 'http://my-bucket.s3.amazonaws.com',
+  :notify_url   => 'http://my.host.com/streams'
 }
 
